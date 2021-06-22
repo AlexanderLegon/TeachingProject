92c740761776d9b1f3bd93adcbc7df41b81c4774
AlexanderLegon <alexlegon2014@gmail.com>
2021-06-22 17:05:16 +0100

    Updated Website

--- src/main/java/com/sparta/alegon/TeachingProject/servlets/IfElseServlet.java
+++ src/main/java/com/sparta/alegon/TeachingProject/servlets/IfElseServlet.java
@@ -13,26 +13,78 @@ import java.io.IOException;
 public class IfElseServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         processRequest(req,resp);
     }
 
     private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String number = req.getParameter("numbers");
+        String number2 = req.getParameter("numbers2");
+        String number3 = req.getParameter("numbers3");
+        String number4 = req.getParameter("numbers4");
+        String number5 = req.getParameter("numbers5");
+        String OutputTo = "ifelse.jsp";
 
-        int number = Integer.getInteger(req.getParameter("number"));
 
-        String OutputTo = "IfElse.jsp";
-        String output;
-        if(number == 10){
+        if(number != null){
+            String output;
+        if(number.equals("10")){
             output = "True";
         }else{
             output = "False";
         }
         RequestDispatcher dispatcher = req.getRequestDispatcher(OutputTo);
         req.setAttribute("output", output);
-        dispatcher.forward(req, resp);
+        dispatcher.forward(req, resp);}
+
+        if(number2 != null){
+            String output2;
+            int temp = Integer.parseInt(number2);
+            if(temp>10){
+                output2 = "True";
+            }else{
+                output2 = "False";
+            }
+            RequestDispatcher dispatcher = req.getRequestDispatcher(OutputTo);
+            req.setAttribute("output2", output2);
+            dispatcher.forward(req, resp);}
+
+        if(number3 != null){
+            String output3;
+            int temp = Integer.parseInt(number3);
+            if(temp>10){
+                output3 = "True";
+            }else{
+                output3 = "False";
+            }
+            RequestDispatcher dispatcher = req.getRequestDispatcher(OutputTo);
+            req.setAttribute("output3", output3);
+            dispatcher.forward(req, resp);}
+        if(number4 != null){
+            String output4;
+            int temp = Integer.parseInt(number4);
+            if(temp<10){
+                output4 = "True";
+            }else{
+                output4 = "False";
+            }
+            RequestDispatcher dispatcher = req.getRequestDispatcher(OutputTo);
+            req.setAttribute("output4", output4);
+            dispatcher.forward(req, resp);}
+
+        if(number5 != null){
+            String output5;
+            int temp = Integer.parseInt(number5);
+            if(temp!=10){
+                output5 = "True";
+            }else{
+                output5 = "False";
+            }
+            RequestDispatcher dispatcher = req.getRequestDispatcher(OutputTo);
+            req.setAttribute("output5", output5);
+            dispatcher.forward(req, resp);}
+        }
 
     }
-}
